0e3f073f86e2e4f7a51972fc4a5bf74ef3eff959
allthatilk <imogenkutz@hotmail.com>
2017-02-12 12:30:11 +0000

    Fizzbuzz working

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "16" for the number 16' do
+    expect(fizzbuzz(16)).to eq 16
+  end
 end
